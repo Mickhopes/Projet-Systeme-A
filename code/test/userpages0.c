cd4cb41ae2299ac425ad493d740fde250cc5022a
monnetpa <monnetpa@im2ag-mandelbrot.e.ujf-grenoble.fr>
2016-01-12 11:26:47 +0100

    Ajout fichier de test partie 4

---
+++ code/test/userpages0.c
@@ -0,0 +1,26 @@
+#include "syscall.h"
+#include "errorno.h"
+#define NULL 0
+
+
+void print(void* arg) {
+	
+	int i;
+	for (i = 0; i < 10; i++){
+		PutChar((int)arg + i);	
+	}
+	UserThreadExit();
+}
+
+
+int main(){
+
+	if((UserThreadCreate(print, (void*)'A')) != -1){
+
+	}
+	if(UserThreadCreate(print, (void*)'a') != -1){
+		
+	}
+	
+	return 0;
+}
