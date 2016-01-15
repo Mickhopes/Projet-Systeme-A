5f0578df9d68a6a15eeedbddc6838f34a7c399d4
Mickhopes <mickael.turnel@gmail.com>
2016-01-15 15:53:16 +0100

    Ajout codes d'erreurs pour les processus
    - EINEX, l'exécutable n'existe pas

--- code/userprog/errorno.h
+++ code/userprog/errorno.h
@@ -1,11 +1,12 @@
 #ifndef ERRORNO_H
 #define ERRORNO_H
 
-#define EDEADLK 1
-#define EINVAL 2
-#define ESRCH 3
-#define ESPACE 4
-#define EMAXTHREADS 5
+#define EDEADLK 1		// A deadlock has been detected
+#define EINVAL 2		// A thread wanted to join a thread already waited
+#define ESRCH 3			// The wanted thread to be joined does not exist
+#define ESPACE 4		// Not enough space left for user thread creation 
+#define EMAXTHREADS 5	// Too much thread running at a time
+#define EINEX 6			// The executable file to be executed by a process does not exist
 
 // Global Variables
 extern unsigned int errorno;
