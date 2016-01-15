89dddb00065210f78a2d0e94de9980df83f6eba9
Mickhopes <mickael.turnel@gmail.com>
2016-01-15 14:21:30 +0100

    Remise de testForkExec par défaut

--- code/test/testForkExec.c
+++ code/test/testForkExec.c
@@ -3,18 +3,12 @@
 int main(){
 
 	PutString("Création premier processus : ");
-	PutInt(ForkExec("glacier"));
-	PutChar('\n');
-
-	/*PutString("Création deuxième processus : ");
 	PutInt(ForkExec("userpages0"));
 	PutChar('\n');
 
-	int i = 0;
-	int sum = 0;
-	for(i = 0; i < 10000; i++) {
-		sum += i/2;
-	}*/
+	PutString("Création deuxième processus : ");
+	PutInt(ForkExec("userpages1"));
+	PutChar('\n');
 
 	return 0;
 }
