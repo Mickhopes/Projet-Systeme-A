668243255af7c98c2fa6e58f4e5e7087a0b1debf
Mickhopes <mickael.turnel@gmail.com>
2016-01-26 16:53:32 +0100

    Nouveau programme de test pour les deadlocks

---
+++ code/test/makethreadsjoin_d.c
@@ -0,0 +1,64 @@
+#include "syscall.h"
+#include "errorno.h"
+#define NULL 0
+
+
+void attendre(void* arg) {
+	
+	PutString("J'attends le thread #");
+	PutInt((int) arg);
+	PutChar('\n');
+
+	if (UserThreadJoin((int) arg) == -1) {
+		int error = GetErrorNo();
+		if (error == ESRCH) {
+			PutString("Thread introuvable\n");
+		} else if (error == EDEADLK) {
+			PutString("Deadlock detecte\n");
+		}
+	} else {
+		PutString("Le thread ");
+		PutInt((int)arg);
+		PutString(" s'est termine avant celui-ci (");
+		PutInt(GetTid());
+		PutString(").\n");
+	}
+}	
+
+void print(void *arg){
+	int i = 0;
+	while(i<100)
+	{
+		PutChar('.');
+		i++;
+	}
+	PutChar('\n');
+}
+
+
+int main(){
+	int id;
+
+	if((id = UserThreadCreate(print, NULL)) == -1){
+		PutString("Erreur lors de la création du thread: ");
+		PutInt(GetErrorNo());
+		PutChar('\n');
+	}
+	if(UserThreadCreate(attendre,(void*) 22) == -1){
+		PutString("Erreur lors de la création du thread: ");
+		PutInt(GetErrorNo());
+		PutChar('\n');
+	}
+	if(UserThreadCreate(attendre,(void*) 4) == -1){
+		PutString("Erreur lors de la création du thread: ");
+		PutInt(GetErrorNo());
+		PutChar('\n');
+	}
+	if(UserThreadCreate(attendre,(void*) 3) == -1){
+		PutString("Erreur lors de la création du thread: ");
+		PutInt(GetErrorNo());
+		PutChar('\n');
+	}
+	
+	return 0;
+}
